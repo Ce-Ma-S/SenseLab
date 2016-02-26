fc2bd1d20222eaee7970f9cc0d387b511cb3d0b5
Marek Ištvánek <marek.istvanek@ce-ma-s.net>
2016-02-26 13:25:04 +0100

    AllJoyn experiment

---
+++ SenseLab.AllJoyn/ObjectProvider.cs
@@ -0,0 +1,15 @@
+﻿using DeviceProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace SenseLab.AllJoyn
+{
+    public class ObjectProvider
+    {
+        public AllJoynProvider Provider { get; private set; }
+    }
+}
